f4325a16af197dce870b88d30f3ad36cc68227ae
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2018-10-02 14:59:14 +0900

    Started implementing background music.
    It's far from over but it gives a good idea.
    Some code should be discarded as it's not used.
    Also, all the other activities should share the Action Bar and its
    behavior.
    Most likely that the playerAdapter instance and its related data (like isBackgroundMusicOn and backgroundMusicPos) should be stored in
    KankenApplication.
    At this moment, on this version, the music stops playing when the
    application is not active.  To make the music keep playing even while
    the user uses a different application, it's recommended to use a service
    instead.  I'm not sure if it's a good idea for now.

---
+++ app/src/main/java/jp/kyoto/nlp/kanken/MediaPlayerHolder.java
@@ -0,0 +1,208 @@
+package jp.kyoto.nlp.kanken;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.util.Log;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public final class MediaPlayerHolder implements PlayerAdapter {
+
+    public static final int PLAYBACK_POSITION_REFRESH_INTERVAL_MS = 1000;
+
+    public MediaPlayerHolder(Context context) {
+        this.context = context.getApplicationContext();
+    }
+
+    /**
+     * Once the {@link MediaPlayer} is released, it can't be used again, and another one has to be
+     * created. In the onStop() method of the {@link MainActivity} the {@link MediaPlayer} is
+     * released. Then in the onStart() of the {@link MainActivity} a new {@link MediaPlayer}
+     * object has to be created. That's why this method is private, and called by load(int) and
+     * not the constructor.
+     */
+    private void initializeMediaPlayer() {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    stopUpdatingCallbackWithPosition(true);
+                    Log.d(tag, "MediaPlayer playback completed");
+                    if (playbackInfoListener != null) {
+                        playbackInfoListener.onStateChanged(PlaybackInfoListener.State.COMPLETED);
+                        playbackInfoListener.onPlaybackCompleted();
+                    }
+                }
+            });
+            Log.d(tag, "mediaPlayer = new MediaPlayer()");
+        }
+    }
+
+    public void setPlaybackInfoListener(PlaybackInfoListener listener) {
+        playbackInfoListener = listener;
+    }
+
+    // Implements PlaybackControl.
+    @Override
+    public void loadMedia(int resourceId) {
+        resId = resourceId;
+
+        initializeMediaPlayer();
+
+        AssetFileDescriptor assetFileDescriptor =
+                context.getResources().openRawResourceFd(resId);
+        try {
+            Log.d(tag, "load() {1. setDataSource}");
+            mediaPlayer.setDataSource(assetFileDescriptor);
+        } catch (Exception e) {
+            Log.d(tag, e.toString());
+        }
+
+        try {
+            Log.d(tag, "load() {2. prepare}");
+            mediaPlayer.prepare();
+        } catch (Exception e) {
+            Log.d(tag, e.toString());
+        }
+
+        initializeProgressCallback();
+        Log.d(tag, "initializeProgressCallback()");
+    }
+
+    @Override
+    public void release() {
+        if (mediaPlayer != null) {
+            Log.d(tag, "release() and mediaPlayer = null");
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if (mediaPlayer != null) {
+            return mediaPlayer.isPlaying();
+        }
+        return false;
+    }
+
+    @Override
+    public void play() {
+        if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
+            Log.d(tag, String.format("playbackStart() %s",
+                                  context.getResources().getResourceEntryName(resId)));
+            mediaPlayer.start();
+            if (playbackInfoListener != null) {
+                playbackInfoListener.onStateChanged(PlaybackInfoListener.State.PLAYING);
+            }
+            startUpdatingCallbackWithPosition();
+        }
+    }
+
+    @Override
+    public void reset() {
+        if (mediaPlayer != null) {
+            Log.d(tag, "playbackReset()");
+            mediaPlayer.reset();
+            loadMedia(resId);
+            if (playbackInfoListener != null) {
+                playbackInfoListener.onStateChanged(PlaybackInfoListener.State.RESET);
+            }
+            stopUpdatingCallbackWithPosition(true);
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            if (playbackInfoListener != null) {
+                playbackInfoListener.onStateChanged(PlaybackInfoListener.State.PAUSED);
+            }
+            Log.d(tag, "playbackPause()");
+        }
+    }
+
+    @Override
+    public void seekTo(int position) {
+        if (mediaPlayer != null) {
+            Log.d(tag, String.format("seekTo() %d ms", position));
+            mediaPlayer.seekTo(position);
+        }
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return (mediaPlayer != null ? mediaPlayer.getCurrentPosition() : -1);
+    }
+
+    /**
+     * Syncs the mediaPlayer position with mPlaybackProgressCallback via recurring task.
+     */
+    private void startUpdatingCallbackWithPosition() {
+        if (scheduleExecuter == null) {
+            scheduleExecuter = Executors.newSingleThreadScheduledExecutor();
+        }
+        //if (mSeekbarPositionUpdateTask == null) {
+        //    mSeekbarPositionUpdateTask = new Runnable() {
+        //        @Override
+        //        public void run() {
+        //            updateProgressCallbackTask();
+        //        }
+        //    };
+        //}
+        //scheduleExecuter.scheduleAtFixedRate(
+        //        mSeekbarPositionUpdateTask,
+        //        0,
+        //        PLAYBACK_POSITION_REFRESH_INTERVAL_MS,
+        //        TimeUnit.MILLISECONDS
+        //);
+    }
+
+    // Reports media playback position to mPlaybackProgressCallback.
+    private void stopUpdatingCallbackWithPosition(boolean resetUIPlaybackPosition) {
+        if (scheduleExecuter != null) {
+            scheduleExecuter.shutdownNow();
+            scheduleExecuter = null;
+            //mSeekbarPositionUpdateTask = null;
+            if (resetUIPlaybackPosition && playbackInfoListener != null) {
+                playbackInfoListener.onPositionChanged(0);
+            }
+        }
+    }
+
+    private void updateProgressCallbackTask() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            int currentPosition = mediaPlayer.getCurrentPosition();
+            if (playbackInfoListener != null) {
+                playbackInfoListener.onPositionChanged(currentPosition);
+            }
+        }
+    }
+
+    @Override
+    public void initializeProgressCallback() {
+        final int duration = mediaPlayer.getDuration();
+        if (playbackInfoListener != null) {
+            playbackInfoListener.onDurationChanged(duration);
+            playbackInfoListener.onPositionChanged(0);
+            Log.d(tag, String.format("firing setPlaybackDuration(%d sec)",
+                                  TimeUnit.MILLISECONDS.toSeconds(duration)));
+            Log.d(tag, "firing setPlaybackPosition(0)");
+        }
+    }
+
+    private final Context context;
+    private MediaPlayer mediaPlayer;
+    private int resId;
+    private PlaybackInfoListener playbackInfoListener;
+    private ScheduledExecutorService scheduleExecuter;
+    //private Runnable mSeekbarPositionUpdateTask;
+
+    private final static String tag = "MediaPlayerHolder";
+
+}
--- app/src/main/java/jp/kyoto/nlp/kanken/WritingProblemActivity.java
+++ app/src/main/java/jp/kyoto/nlp/kanken/WritingProblemActivity.java
@@ -5,9 +5,11 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.ActionBar;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -172,10 +174,36 @@ public class WritingProblemActivity extends QuizProblemActivity {
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setTitle(getResources().getString(R.string.app_name));
         actionBar.show();
+
+        initializePlaybackController();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        playerAdapter.loadMedia(R.raw.background_music);
+        if (backgroundMusicPos != -1)
+            playerAdapter.seekTo(backgroundMusicPos);
+        if (isBackgroundMusicOn)
+            playerAdapter.play();
+        Log.d(tag, "onStart: create MediaPlayer");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isChangingConfigurations() && playerAdapter.isPlaying()) {
+            Log.d(tag, "onStop: don't release MediaPlayer as screen is rotating & playing");
+        } else {
+            backgroundMusicPos = playerAdapter.getCurrentPosition();
+            playerAdapter.release();
+            Log.d(tag, "onStop: release MediaPlayer");
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        actionBarMenu = menu;
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.action_bar_writing_problem_menu, menu);
         return super.onCreateOptionsMenu(menu);
@@ -184,14 +212,22 @@ public class WritingProblemActivity extends QuizProblemActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
-        String menuTitle = (String)item.getTitle();
-
-        StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("You clicked menu ");
-        stringBuffer.append(menuTitle);
-
-        String message = stringBuffer.toString();
-
+        if (itemId == R.id.menuToggleMusic) {
+            if (isBackgroundMusicOn) {
+                actionBarMenu.getItem(0).setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.music_off, null));
+                isBackgroundMusicOn = false;
+                // Also save this information as a preference.
+                playerAdapter.pause();
+            }
+            else {
+                actionBarMenu.getItem(0).setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.music_on, null));
+                isBackgroundMusicOn = true;
+                // Also save this information as a preference.
+                playerAdapter.play();
+                //MediaPlayer mediaPlayer = MediaPlayer.create(WritingProblemActivity.this, R.raw.background_music);
+                //mediaPlayer.start();
+            }
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -426,6 +462,14 @@ public class WritingProblemActivity extends QuizProblemActivity {
     private void loaded() {
         KanjiDrawing kanjiCanvas = findViewById(R.id.kanjiDrawing);
         DrawnStroke[] strokes = kanjiCanvas.getStrokes();
+    }
+
+    private void initializePlaybackController() {
+        MediaPlayerHolder mediaPlayerHolder = new MediaPlayerHolder(this);
+        Log.d(tag, "initializePlaybackController: created MediaPlayerHolder");
+        mediaPlayerHolder.setPlaybackInfoListener(new PlaybackListener());
+        playerAdapter = mediaPlayerHolder;
+        Log.d(tag, "initializePlaybackController: MediaPlayerHolder progress callback set");
     }
 
     /**
@@ -907,6 +951,50 @@ public class WritingProblemActivity extends QuizProblemActivity {
 
     }
 
+    public class PlaybackListener extends PlaybackInfoListener {
+
+        @Override
+        public void onDurationChanged(int duration) {
+            //mSeekbarAudio.setMax(duration);
+            Log.d(tag, String.format("setPlaybackDuration: setMax(%d)", duration));
+        }
+
+        @Override
+        public void onPositionChanged(int position) {
+            //if (!mUserIsSeeking) {
+            //    mSeekbarAudio.setProgress(position, true);
+            //    Log.d(tag, String.format("setPlaybackPosition: setProgress(%d)", position));
+            //}
+        }
+
+        @Override
+        public void onStateChanged(@State int state) {
+            //String stateToString = PlaybackInfoListener.convertStateToString(state);
+            //onLogUpdated(String.format("onStateChanged(%s)", stateToString));
+        }
+
+        @Override
+        public void onPlaybackCompleted() {
+        }
+
+        @Override
+        public void onLogUpdated(String message) {
+            //if (mTextDebug != null) {
+            //    mTextDebug.append(message);
+            //    mTextDebug.append("\n");
+            //    // Moves the scrollContainer focus to the end.
+            //    mScrollContainer.post(
+            //            new Runnable() {
+            //                @Override
+            //                public void run() {
+            //                    mScrollContainer.fullScroll(ScrollView.FOCUS_DOWN);
+            //                }
+            //            });
+            //}
+        }
+
+    }
+
     private KankenApplication appl = KankenApplication.getInstance();
 
     private LinearLayout layoutKanjiInputRight_a; 
@@ -915,8 +1003,13 @@ public class WritingProblemActivity extends QuizProblemActivity {
     private ImageView imageViewSearchingWritingProblemCharacter;
     private TextView textViewProblemUserAnswer;
     private ActionBar actionBar;
+    private Menu actionBarMenu;
 
     private Typeface typefaceKanjiButton;
+
+    private PlayerAdapter playerAdapter; 
+    private int backgroundMusicPos = -1;
+    private boolean isBackgroundMusicOn = true;
 
     private int kanjiPage = 0;
     private String[] kanjis;
