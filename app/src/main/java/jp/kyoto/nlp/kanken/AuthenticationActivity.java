d41809728523f99abd29321a0f6a91fb94a246e1
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2018-10-03 16:30:39 +0900

    Removed unused stuff.

--- app/src/main/java/jp/kyoto/nlp/kanken/AuthenticationActivity.java
+++ app/src/main/java/jp/kyoto/nlp/kanken/AuthenticationActivity.java
@@ -13,12 +13,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -26,8 +21,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -51,55 +44,12 @@ public class AuthenticationActivity extends AppCompatActivity implements View.On
         startActivity(quizSettingsActivity);
     }
 
-    public void signOut(android.view.View view) {
-        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(
-            new ResultCallback<Status>() {
-                @Override
-                public void onResult(@NonNull Status status) {
-                    URL signOutUrl;
-                    try {
-                        signOutUrl = new URL(appl.getServerBaseUrl() + signOutReqPath);
-
-                        progressDialog = new ProgressDialog(AuthenticationActivity.this);
-                        progressDialog.setMessage(getResources().getString(R.string.label_signing_out));
-                        progressDialog.setCancelable(false);
-                        progressDialog.show();
-
-                        new SignOutTask().execute(signOutUrl);
-                    }
-                    catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    }
-                    catch(IOException e2) {
-                        e2.printStackTrace();
-                    }
-                    catch(JSONException e3) {
-                        e3.printStackTrace();
-                    }
-                }
-            }
-        );
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
 
-        layoutUserInfo = findViewById(R.id.layoutUserInfo);
-        layoutLogin = findViewById(R.id.layoutLogin);
-        Button buttonSignOut = findViewById(R.id.buttonSignOut);
         SignInButton buttonSignIn = findViewById(R.id.buttonSignIn);
-        textViewUserName = findViewById(R.id.textViewUserName);
-        textViewUserEmail = findViewById(R.id.textViewUserEmail);
-        imageViewUserPicture = findViewById(R.id.imageViewUserPicture);
-        TextView textViewAuthenticationInfoTitle = findViewById(R.id.textViewAuthenticationInfoTitle);
-        TextView textViewAuthenticationInfo = findViewById(R.id.textViewAuthenticationInfo);
-
-        // No title needed for now.  Delete it eventually.
-        textViewAuthenticationInfoTitle.setVisibility(View.GONE);
-
-        layoutUserInfo.setVisibility(View.GONE);
 
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(Util.googleClientId).requestEmail().build();
         googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
@@ -123,15 +73,11 @@ public class AuthenticationActivity extends AppCompatActivity implements View.On
             String idToken = account.getIdToken();
 
             Uri pictureUrl = account.getPhotoUrl();
-            textViewUserName.setText(name);
-            textViewUserEmail.setText(email);
            
             appl.setUserName(name);
             appl.setUserEmail(email);
             appl.setUserPictureUrl(pictureUrl);
             appl.setUserIdToken(idToken);
-
-            Glide.with(this).load(pictureUrl).into(imageViewUserPicture);
 
             URL signInUrl;
             try {
@@ -157,19 +103,6 @@ public class AuthenticationActivity extends AppCompatActivity implements View.On
         else {
             appl.setUserName(null);
             appl.setUserEmail(null);
-
-            updateUI(false);
-        }
-    }
-
-    private void updateUI(boolean isAuthenticated) {
-        if (isAuthenticated) {
-            layoutUserInfo.setVisibility(View.VISIBLE);
-            layoutLogin.setVisibility(View.GONE);
-        }
-        else {
-            layoutUserInfo.setVisibility(View.GONE);
-            layoutLogin.setVisibility(View.VISIBLE);
         }
     }
 
@@ -307,94 +240,12 @@ public class AuthenticationActivity extends AppCompatActivity implements View.On
                 return;
             }
 
-            //updateUI(true);
             play(null);
         }
 
         private Exception exception;
 
     }
-
-    private class SignOutTask extends AsyncTask {
-
-        protected Object doInBackground(Object... objs) {
-            URL signOutUrl = (URL)objs[0];
-            try {
-                HttpURLConnection con = (HttpURLConnection) signOutUrl.openConnection();
-                con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-                con.setRequestProperty("Accept", "application/json");
-                con.setRequestMethod("POST");
-                    con.setFixedLengthStreamingMode(0);
-                con.connect();
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-                
-                JSONObject jsonResponse = new JSONObject(response.toString());
-                String status = jsonResponse.getString("status");
-                Log.d(tag, "status=" + status);
-                if (!"ok".equals(status))
-                    exception = new Exception("Server responded with status=" + status + ". Something is probably wrong.");
-
-                return null;
-            }
-            catch(IOException e) {
-                e.printStackTrace(); 
-                exception = e;
-            }
-            catch(JSONException e2) {
-                e2.printStackTrace(); 
-                exception = e2;
-            }
-
-            return null;
-        }
-
-        protected void onPostExecute(final Object obj) {
-            if (progressDialog != null) {
-                progressDialog.dismiss();
-                progressDialog = null;
-            }
-
-            if (exception != null) {
-                Log.e(tag, "An exception has occurred: " + exception);
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(AuthenticationActivity.this);
-                builder.setTitle(getResources().getString(R.string.error_server_unreachable_title))
-                .setMessage(getResources().getString(R.string.error_server_unreachable_msg))
-                .setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                 })
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setCancelable(true)
-                .show();
-
-                return;
-            }
-
-            appl.setUserName(null);
-            appl.setUserEmail(null);
-            appl.setUserIdToken(null);
-            appl.setSessionCookie(null);
-
-            updateUI(false);
-        }
-
-        private Exception exception;
-
-    }
-
-    private LinearLayout layoutUserInfo;
-    private TextView textViewUserName;
-    private TextView textViewUserEmail;
-    private ImageView imageViewUserPicture;
-    private LinearLayout layoutLogin;
 
     private GoogleApiClient googleApiClient;
 
@@ -405,7 +256,6 @@ public class AuthenticationActivity extends AppCompatActivity implements View.On
     private static final int REQ_CODE = 9001;
    
     private static final String signInReqPath = "/cgi-bin/sign_in.cgi";
-    private static final String signOutReqPath = "/cgi-bin/sign_out.cgi";
 
     private static final String tag = "AuthenticationActivity";
 
